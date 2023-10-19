e35f4d9c3d4b202dc18686aa771de691ce2fedb4
Marcin Patoń <mpaton@inpost.pl>
2023-10-19 10:59:17 +0200

    add swagger

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
+@OpenAPIDefinition(info = @Info(
+    title = "REST services",
+    description = "",
+    version = "1.0.0"
+))
 public class HelloApplication extends Application {
 
 }
